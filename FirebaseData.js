a6adb5da2044bb26bdbc962bc02d88e5918ca816
AmirSameh1 <amirsameh770@gmail.com>
2025-03-13 10:47:42 +0200

    Update FirebaseData.js

--- FirebaseData.js
+++ FirebaseData.js
@@ -11,19 +11,14 @@ import { getDatabase, ref, onValue, onChildAdded } from "https://www.gstatic.com
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyC-D17GV3prkmQDssF9ZbXiKGQq2-XLsiE",
-
-  authDomain: "project-0-51c9c.firebaseapp.com",
-
-  databaseURL: "https://project-0-51c9c-default-rtdb.firebaseio.com",
-
-  projectId: "project-0-51c9c",
-
-  storageBucket: "project-0-51c9c.firebasestorage.app",
-
-  messagingSenderId: "486817466192",
-
-  appId: "1:486817466192:web:108b7eadfc3b6fe3604e55"
+  apiKey: "AIzaSyClMKnJj5AMrkzfJscHRBwRyXXsRQUzUaU",
+  authDomain: "water-level-sensor-data-78d28.firebaseapp.com",
+  databaseURL: "https://water-level-sensor-data-78d28-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "water-level-sensor-data-78d28",
+  storageBucket: "water-level-sensor-data-78d28.firebasestorage.app",
+  messagingSenderId: "442646995845",
+  appId: "1:442646995845:web:9309293caa66d25a29daec",
+  measurementId: "G-7QT1V26PK5"
 
 
 };
